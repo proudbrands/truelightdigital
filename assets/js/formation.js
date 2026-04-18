215e9e974f1c81ea2916ac5d234796e54897f9d9
proudbrands <sean@proudbrands.co.uk>
2026-04-19 00:29:30 +0100

    Bump scroll-margin-top to 180px (mobile 140px); nav is ~140px tall with breathing room

--- assets/js/formation.js
+++ assets/js/formation.js
@@ -39,7 +39,7 @@
           match.link.parentElement.classList.add('is-active');
         }
       });
-    }, { rootMargin: '-140px 0px -55% 0px', threshold: 0 });
+    }, { rootMargin: '-190px 0px -55% 0px', threshold: 0 });
 
     targets.forEach(function (t) { observer.observe(t.el); });
   }
